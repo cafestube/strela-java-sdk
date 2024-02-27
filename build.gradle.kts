9b23e6d6dece69c08b7ddb52ed780cf1e57102bd
Philipp <p.eistrach@gmail.com>
2024-02-27 17:20:10 +0100

    feat: set up maven central upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,11 @@ plugins {
   id("java")
   alias(libs.plugins.shadow)
   alias(libs.plugins.fabricJavaGenerator)
+  alias(libs.plugins.sonatypeCentralPortalPublisher)
 }
 
 group = "dev.strela"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
   mavenCentral()
@@ -32,6 +33,47 @@ sourceSets {
       srcDirs("build/generated/sources/dev/strela/v1")
     }
   }
+}
+
+tasks.withType<Javadoc> {
+  source = files(source).minus(fileTree("build/generated/sources")).asFileTree
+}
+
+centralPortal {
+  username = project.findProperty("sonatypeUsername") as String
+  password = project.findProperty("sonatypePassword") as String
+
+  pom {
+    name.set("Strela Java SDK")
+    description.set("A Java SDK for Strela")
+    url.set("https://github.com/strela-dev/java-sdk")
+
+    developers {
+      developer {
+        id.set("fllipeis")
+        email.set("p.eistrach@gmail.com")
+      }
+    }
+    licenses {
+      license {
+        name.set("Apache-2.0")
+        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+      }
+    }
+    scm {
+      url.set("https://github.com/strela-dev/java-sdk.git")
+      connection.set("git:git@github.com:strela-dev/java-sdk.git")
+    }
+  }
+}
+
+signing {
+  useGpgCmd()
+  sign(configurations.archives.get())
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks.test {
